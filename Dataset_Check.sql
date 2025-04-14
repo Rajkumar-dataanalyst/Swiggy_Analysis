5e5fa4bcd984d0ed0bc2da30bb3bc931c3b7bf48
Rajkumar-dataanalyst <rajkumarswarna973@gmail.com>
2025-04-14 11:25:27 +0530

    Add files via upload

---
+++ Dataset_Check.sql
@@ -0,0 +1,9 @@
+create database swiggy_dashboard_db;
+select * from customers;
+select * from customeraddress;
+select * from delivery;
+select * from deliveryagent;
+select * from `login audit`;
+select * from menu;
+select * from orders;
+select * from restaurant;   
